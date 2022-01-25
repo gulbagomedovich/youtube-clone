7fdd39bb82c01102207f8a6416f52efbf54a6e6f
gulbagomedovich <gulbagomedovich@gmail.com>
2022-01-26 00:05:09 +0300

    Добавил каркас проекта

---
+++ backend/youtube-clone-service/src/main/java/com/gulbagomedovich/youtubecloneservice/YoutubeCloneServiceApplication.java
@@ -0,0 +1,13 @@
+package com.gulbagomedovich.youtubecloneservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeCloneServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeCloneServiceApplication.class, args);
+	}
+
+}
---
+++ backend/youtube-clone-service/src/test/java/com/gulbagomedovich/youtubecloneservice/YoutubeCloneServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gulbagomedovich.youtubecloneservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeCloneServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
